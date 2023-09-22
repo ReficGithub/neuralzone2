669725974cba2adb019f3ba5fac8cd5ee8750e70
Stefan <39063902+ReficGithub@users.noreply.github.com>
2023-09-22 13:28:00 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,8 @@
 import yfinance as yf
 
 symbool = "^SPX"
-startdatum = 2020-01-01
-einddatum = 2023-01-01
+startdatum = "2020-01-01"
+einddatum = "2023-01-01"
 
 financiele_gegevens = haal_financiele_gegevens_op(symbool, startdatum, einddatum)
 print(financiele_gegevens)
