338700c9f0f4d6809bdd2806a72ce3209f6ba046
Stefan <39063902+ReficGithub@users.noreply.github.com>
2023-09-22 13:28:56 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,5 +5,9 @@ symbool = "^SPX"
 startdatum = "2020-01-01"
 einddatum = "2023-01-01"
 
+def haal_financiele_gegevens_op(symbool, startdatum, einddatum):
+    df = yf.download(symbool, start=startdatum, end=einddatum)
+    return df
+
 financiele_gegevens = haal_financiele_gegevens_op(symbool, startdatum, einddatum)
 print(financiele_gegevens)
